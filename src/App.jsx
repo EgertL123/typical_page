aee418e07a79e3d46b3d3f85133c8e98c54e81ab
Egert Linnas <egert.linnas@voco.ee>
2025-03-22 15:46:45 +0200

    3. Projekti täiendamine useContext hooki kasutamisega

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
-const App = () => {
+import AuthContext from "./store/auth-context";
+
+function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(() => {
 		const storedUser = localStorage.getItem('isLoggedUser');
 		return storedUser ? JSON.parse(storedUser).isLogged : false;
@@ -31,13 +33,18 @@ const App = () => {
 	};
 
 	return (
-		<>
-			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+		<AuthContext.Provider value={
+      { 
+        loggedIn: isLoggedIn,
+        onLogout: logoutHandler
+      }
+        }>
+			<MainHeader onLogout={logoutHandler} />
 			<main>
 				{!isLoggedIn && <Login onLogin={loginHandler} />}
 				{isLoggedIn && <Home />}
 			</main>
-		</>
+		</AuthContext.Provider>
 	);
 }
 
--- src/components/MainHeader/Navigation.jsx
+++ src/components/MainHeader/Navigation.jsx
@@ -1,27 +1,33 @@
 import './Navigation.css';
-
 import Button from '../UI/Button';
+
+import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
     return (
+        <AuthContext.Consumer>
+            {(context) => {
+                return (
         <nav className="nav">
             <ul>
                 <li>
-                    <a href="/">Users</a>
+                    <a href="/users">Users</a>
                 </li>
                 <li>
-                    <a href="/new-meetup">Admin</a>
+                    <a href="/admin">Admin</a>
                 </li>
                 {
-                    props.isLoggedIn && (
+                   context.loggedIn && (
                         <li>
                             <Button onClick={props.onLogout}> Logout </Button>
                         </li>
-                    )
-                }
+                    )}
             </ul>
         </nav>
     );
+}}
+</AuthContext.Consumer>
+    )
 }
 
 export default Navigation;
